e32fe47afca3a20bcdbfbec5d81a34d12bdd2931
harsha1a <72252136+harsha1a@users.noreply.github.com>
2021-01-18 23:15:44 +0530

    Add files via upload

---
+++ pgm9.php
@@ -0,0 +1,29 @@
+<?php
+$states="mississippi alabama texas massachusetts Kansas"; 
+$statesArray=[];
+$states1=explode(' ',$states);
+echo"original Array:<br>";
+foreach($states1 as $i=>$value)
+print("STATES[$i]=>$value<br>");
+foreach($states1 as $i=>$value)
+print("STATES[$i]=$value<br>");
+foreach($states1 as $state) {
+    if(preg_match('/xas$/',($state)))
+$statesArray[0]=($state);
+}
+foreach($states1 as $state) {
+    if(preg_match('/^k.*s$/i',($state)))
+    $statesArray[1]=($state);
+}
+foreach($states1 as $state) {
+    if(preg_match('/^M.*$/',($state)))
+$statesArray[2]=($state);
+}
+foreach($state1 as $state){
+    if(preg_match('/a$/',($state)))
+    $statesArray[3]=(state);
+}
+echo"<br><br>Resultant Array:<br>";
+foreach($statesArray as $Array=>$value)
+print("STATES[$Array]=$value<br>");
+?>
